93464e21d89b36a87c6cfc974061a4a52672c145
Han Wang <wang_han@iapcm.ac.cn>
2019-04-08 23:29:42 +0800

    test multiple dump frames

--- tests/test_dump_to_system.py
+++ tests/test_dump_to_system.py
@@ -11,6 +11,19 @@ class TestDump(unittest.TestCase, TestPOSCARoh):
         self.system.from_lammps_dump(os.path.join('poscars', 'conf.dump'), 
                                      type_map = ['O', 'H'])
         
+class TestDump2(unittest.TestCase, TestPOSCARoh):
+    
+    def setUp(self): 
+        self.tmp_system = dpdata.System()
+        self.tmp_system.from_lammps_dump(os.path.join('poscars', 'conf.dump'), 
+                                         type_map = ['O', 'H'])        
+        self.system = dpdata.System()
+        self.system.sub_system(self.tmp_system, [1])
+
+    def test_nframes (self) :
+        self.assertEqual(self.tmp_system.get_nframes(), 2)
+        
+        
 if __name__ == '__main__':
     unittest.main()
     
