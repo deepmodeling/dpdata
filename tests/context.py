74abf1f58687dcf9570d88c804fbeafe589e8289
Han Wang <wang_han@iapcm.ac.cn>
2019-04-10 15:52:32 +0800

    add test of water ion

--- tests/context.py
+++ tests/context.py
@@ -1,3 +1,4 @@
 import sys,os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import dpdata
+import dpdata.md.water
---
+++ tests/test_water_ions.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+import unittest
+from context import dpdata
+
+class TestIons(unittest.TestCase):
+    
+    def setUp(self): 
+        self.system = dpdata.System()
+        self.system.from_lammps_lmp(os.path.join('poscars', 'conf.waterion.lmp'), 
+                                    type_map = ['O', 'H'])
+        self.bonds = dpdata.md.water.compute_bonds(self.system.data['cell'][0],
+                                                   self.system.data['frames'][0],
+                                                   self.system.data['atom_types'])
+
+
+    def test_ions_count(self) :
+        no, noh, noh2, noh3, nh \
+            = dpdata.md.water.find_ions(self.system.data['atom_types'], self.bonds)
+        self.assertEqual(len(no), 0)
+        self.assertEqual(len(noh), 1)
+        self.assertEqual(len(noh2), 125)
+        self.assertEqual(len(noh3), 1)
+        self.assertEqual(len(nh), 0)
+        self.assertEqual(noh[0], 0)
+        self.assertEqual(noh3[0], 51)
+
+        
+if __name__ == '__main__':
+    unittest.main()
+    
