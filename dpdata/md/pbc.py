e55087175e7e9523811288514ed0b40bc4c4c190
Han Wang <wang_han@iapcm.ac.cn>
2019-04-18 14:51:16 +0800

    add system pbc shift

--- dpdata/md/pbc.py
+++ dpdata/md/pbc.py
@@ -31,3 +31,25 @@ def posi_shift(box, r0, r1) :
             shift[dd] += 1
     return shift
 
+
+def dir_coord(coord, box) :
+    rbox = np.linalg.inv(box)
+    return np.matmul(coord, rbox)
+
+
+def system_pbc_shift(system) :
+    f_idx = 0
+    prev_ncoord = dir_coord(system['coords'][f_idx], 
+                            system['cells' ][f_idx])
+    shifts = np.zeros([system.get_nframes(), system.get_natoms(), 3], dtype = int)
+    curr_shift = np.zeros([system.get_natoms(), 3], dtype = int)
+    half = 0.5 * np.ones([system.get_natoms(), 3])
+    for ii in range(system.get_nframes()) :
+        curr_ncoord = dir_coord(system['coords'][ii], 
+                                system['cells' ][ii])
+        diff_ncoord = curr_ncoord - prev_ncoord
+        curr_shift -= (diff_ncoord > half)
+        curr_shift += (diff_ncoord <-half)
+        shifts[ii] = np.copy(curr_shift)
+        prev_ncoord = curr_ncoord
+    return np.array(shifts, dtype = int)
