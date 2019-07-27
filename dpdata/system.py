047ceb8bc6465f3ac7bb7b718850961c07e1ad32
AnguseZhang <529133328@qq.con>
2019-07-27 14:48:43 +0800

    remove pymatgen dependency

--- dpdata/system.py
+++ dpdata/system.py
@@ -13,7 +13,7 @@ import dpdata.gaussian.log
 from copy import deepcopy
 from monty.json import MSONable
 from monty.serialization import loadfn,dumpfn
-from pymatgen import Element
+from mendeleev import element as Element 
 
 class System (MSONable) :
     '''
@@ -867,7 +867,7 @@ def check_LabeledSystem(data):
 
 def elements_index_map(elements,standard=False,inverse=False):
     if standard:
-       elements.sort(key=lambda x: Element(x).number)
+       elements.sort(key=lambda x: Element(x).atomic_number)
     if inverse:
        return dict(zip(range(len(elements)),elements))
     else:
