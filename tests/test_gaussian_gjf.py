2d2d43db4b5069734aef78095424212a5105274e
Ericwang6 <ericwangyz602@gmail.com>
2021-04-28 00:00:28 +0800

    add test for write gjf files

--- tests/test_gaussian_gjf.py
+++ tests/test_gaussian_gjf.py
@@ -10,6 +10,27 @@ class TestDumpGaussianGjf(unittest.TestCase):
     
     def test_dump_to_gjf(self):
         self.system.to_gaussian_gjf("gaussian/tmp.gjf")
+        with open("gaussian/tmp.gjf") as f:
+            header = f.readline().strip()
+            f.readline()
+            title = f.readline().strip()
+            f.readline()
+            charge, mult = (int(x) for x in f.readline().strip().split())
+            atoms = []
+            coords = []
+            for ii in range(5):
+                line = f.readline().strip().split()
+                atoms.append(line[0])
+                coords.append([float(x) for x in line[1:]])
+
+        self.assertEqual(header, "#force B3LYP/6-31G(d)")
+        self.assertEqual(title, self.system.formula)
+        self.assertEqual(charge, 0)
+        self.assertEqual(mult, 1)
+        self.assertEqual(atoms, ['C', 'H', 'H', 'H', 'H'])
+        for i in range(self.system['coords'].shape[1]):
+            for j in range(3):
+                self.assertAlmostEqual(coords[i][j], self.system['coords'][0][i][j])
     
     def tearDown(self):
         if os.path.exists('gaussian/tmp.gjf'):
