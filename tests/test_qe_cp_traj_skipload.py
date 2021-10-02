a8909a26b9813cd065b43152c0f685897d2310e2
Yingze Wang <ericwangyz602@gmail.com>
2021-10-02 19:56:27 +0800

    use unit conversion from unit.py

--- tests/test_qe_cp_traj_skipload.py
+++ tests/test_qe_cp_traj_skipload.py
@@ -6,6 +6,8 @@ from comp_sys import CompSys
 from comp_sys import CompLabeledSys
 from comp_sys import IsPBC
 
+bohr2ang = dpdata.unit.LengthConversion("bohr", "angstrom").value()
+
 class TestPWSCFTrajSkip(unittest.TestCase, CompSys, IsPBC):
     def setUp(self): 
         self.system_1 = dpdata.System(os.path.join('qe.traj', 'traj6'), 
@@ -42,7 +44,7 @@ class TestPWSCFLabeledTrajSkip(unittest.TestCase, CompLabeledSys, IsPBC):
         ref_cell = [5.359985500701728967e+00, 0, 0,
                     3.585941820098031974e-01, 5.317218997480877896e+00, 0,
                     7.606780476053129902e-01, 7.811107228901693622e-01, 5.715864930517207121e+00 ]
-        ref_cell = 0.52917721067 * np.array(ref_cell).reshape(3,3)
+        ref_cell = bohr2ang * np.array(ref_cell).reshape(3,3)
         
         for ii in range(3) :
             for jj in range(3) :
@@ -51,7 +53,7 @@ class TestPWSCFLabeledTrajSkip(unittest.TestCase, CompLabeledSys, IsPBC):
         ref_cell = [5.308510801020571712e+00, 0, 0,
                     3.076052782312116429e-01, 5.279388982187173340e+00, 0,
                     4.321921336152507731e-01, 8.121110815096156399e-01, 5.301664983741235737e+00]
-        ref_cell = 0.52917721067 * np.array(ref_cell).reshape(3,3)
+        ref_cell = bohr2ang * np.array(ref_cell).reshape(3,3)
         
         for ii in range(3) :
             for jj in range(3) :
