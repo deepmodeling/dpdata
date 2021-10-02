f9a62608dc81f0543ca5652b7aba6d856124b347
Yingze Wang <ericwangyz602@gmail.com>
2021-10-02 20:02:13 +0800

    use ELEMENT from dpdata/periodic_table.py

--- dpdata/amber/md.py
+++ dpdata/amber/md.py
@@ -4,13 +4,10 @@ from scipy.io import netcdf
 import numpy as np
 from dpdata.amber.mask import pick_by_amber_mask
 from dpdata.unit import EnergyConversion
+from ..periodic_table import ELEMENTS
 
-# kcalmol2eV= 0.04336410390059322
-# print(kcalmol2eV)
-# cc = EnergyConversion("kcal_mol", "eV")
-# print(cc)
 kcalmol2eV = EnergyConversion("kcal_mol", "eV").value()
-symbols = ['X', 'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+symbols = ['X'] + ELEMENTS
 
 energy_convert = kcalmol2eV
 force_convert = energy_convert
