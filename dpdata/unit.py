fa3a447ec731dd6ed62d504cd4d6dbe85c19f4b8
Yingze Wang <ericwangyz602@gmail.com>
2021-10-03 16:21:25 +0800

    Add examples for conversion

--- dpdata/unit.py
+++ dpdata/unit.py
@@ -1,9 +1,10 @@
 from abc import ABC
 from scipy import constants
 
-AVOGADRO = constants.Avogadro           # Avagadro constant
-ELE_CHG  = constants.elementary_charge  # Elementary Charge, in C
-BOHR     = constants.value("atomic unit of length") # Bohr, in m
+AVOGADRO = constants.Avogadro                             # Avagadro constant
+ELE_CHG  = constants.elementary_charge                    # Elementary Charge, in C
+BOHR     = constants.value("atomic unit of length")       # Bohr, in m
+RYDBERG  = constants.Rydberg * constants.h * constants.c  # Rydberg, in Jole
 
 # energy conversions
 econvs = {
@@ -11,7 +12,7 @@ econvs = {
     "hartree": 4.35974394E-18 / ELE_CHG,
     "kJ_mol": 1 / (ELE_CHG * AVOGADRO / 1000),
     "kcal_mol": 1 / (ELE_CHG * AVOGADRO / 1000 / 4.184),
-    "rydberg": 2.17987197E-18 / ELE_CHG,
+    "rydberg": RYDBERG / ELE_CHG,
     "J": 1 / ELE_CHG,
     "kJ": 1000 / ELE_CHG
 }
@@ -39,7 +40,22 @@ def check_unit(unit):
 class Conversion(ABC):
     def __init__(self, unitA, unitB, check=True):
         """
-        1 unitA = self.value unitB
+        Parent class for unit conversion
+
+        Parameters
+        ----------
+            unitA : str, unit to be converted
+            unitB : str, unit which unitA is converted to, i.e. `1 unitA = self._value unitB`
+            check : bool, whether to check unit validity
+        
+        Examples
+        --------
+        >>> conv = Conversion("foo", "bar", check=False)
+        >>> conv.set_value("10.0")
+        >>> print(conv)
+        >>> "1 foo = 10.0 bar"
+        >>> conv.value()
+        >>> 10.0
         """
         if check:
             check_unit(unitA)
@@ -51,6 +67,9 @@ class Conversion(ABC):
     def value(self):
         return self._value
     
+    def set_value(self, value):
+        self._value = value
+    
     def __repr__(self):
         return f"1 {self.unitA} = {self._value} {self.unitB}"
     
@@ -59,23 +78,67 @@ class Conversion(ABC):
 
 class EnergyConversion(Conversion):
     def __init__(self, unitA, unitB):
+        """
+        Class for energy conversion
+
+        Examples
+        --------
+        >>> conv = LengthConversion("eV", "kcal_mol")
+        >>> conv.value()
+        >>> 23.06054783061903
+        """
         super().__init__(unitA, unitB)
-        self._value = econvs[unitA] / econvs[unitB]
+        self.set_value(econvs[unitA] / econvs[unitB])
 
 class LengthConversion(Conversion):
     def __init__(self, unitA, unitB):
+        """
+        Class for length conversion
+
+        Examples
+        --------
+        >>> conv = LengthConversion("angstrom", "nm")
+        >>> conv.value()
+        >>> 0.1
+        """
         super().__init__(unitA, unitB)
-        self._value = lconvs[unitA] / lconvs[unitB]
+        self.set_value(lconvs[unitA] / lconvs[unitB])
 
 class ForceConversion(Conversion):
     def __init__(self, unitA, unitB):
+        """
+        Class for force conversion
+
+        Parameters
+        ----------
+            unitA, unitB : str, in format of "energy_unit/length_unit"
+
+        Examples
+        --------
+        >>> conv = ForceConversion("kJ_mol/nm", "eV/angstrom")
+        >>> conv.value()
+        >>> 0.0010364269656262175
+        """
         super().__init__(unitA, unitB)
         econv = EnergyConversion(unitA.split("/")[0], unitB.split("/")[0]).value()
         lconv = LengthConversion(unitA.split("/")[1], unitB.split("/")[1]).value()
-        self._value = econv / lconv
+        self.set_value(econv / lconv)
 
 class PressureConversion(Conversion):
     def __init__(self, unitA, unitB):
+        """
+        Class for pressure conversion
+
+        Parameters
+        ----------
+            unitA, unitB : str, in format of "energy_unit/length_unit^3", or in `["Pa", "pa", "kPa", "kpa", "bar", "kbar"]`
+
+        Examples
+        --------
+        >>> conv = PressureConversion("kbar", "eV/angstrom^3")
+        >>> conv.value()
+        >>> 0.0006241509074460763
+        """
         super().__init__(unitA, unitB, check=False)
         unitA, factorA = self._convert_unit(unitA)
         unitB, factorB = self._convert_unit(unitB)
@@ -83,7 +146,7 @@ class PressureConversion(Conversion):
         eunitB, lunitB = self._split_unit(unitB)
         econv = EnergyConversion(eunitA, eunitB).value() * factorA / factorB
         lconv = LengthConversion(lunitA, lunitB).value()
-        self._value = econv / lconv**3
+        self.set_value(econv / lconv**3)
     
     def _convert_unit(self, unit):
         if unit == "Pa" or unit == "pa":
@@ -101,6 +164,3 @@ class PressureConversion(Conversion):
         eunit = unit.split("/")[0]
         lunit = unit.split("/")[1][:-2]
         return eunit, lunit
-
-
-
