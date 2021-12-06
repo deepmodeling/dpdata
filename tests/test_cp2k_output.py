7b7f8ea8cf7de81485e9160ba63e5d0d9e691995
robinzyb <robinzhuang@outlook.com>
2021-12-06 12:58:05 +0800

    fix bug for duplicated header cp2k output

--- tests/test_cp2k_output.py
+++ tests/test_cp2k_output.py
@@ -50,6 +50,11 @@ class TestCP2KLabeledOutput(unittest.TestCase, TestCP2KSinglePointEnergy):
     def setUp (self) :
         self.system = dpdata.LabeledSystem('cp2k/cp2k_output',
                                            fmt = 'cp2k/output')
+                                           
+class TestCP2KDuplicateHeader(unittest.TestCase, TestCP2KSinglePointEnergy):
+    def setUp (self) :
+        self.system = dpdata.LabeledSystem('cp2k/cp2k_output_duplicate_header',
+                                           fmt = 'cp2k/output')                                          
 
 class TestNonCoveragedCP2KOutput(unittest.TestCase):
     def setUp (self) :
