69ee66f8c584fbfc29afe6dae71bcd1ba58400c5
Han Wang <wang_han@iapcm.ac.cn>
2022-02-24 08:33:42 +0800

    skip UTs when we do not have parmed, ase, pymatgen

--- tests/test_ase_traj.py
+++ tests/test_ase_traj.py
@@ -3,23 +3,31 @@ import numpy as np
 import unittest
 from context import dpdata
 from comp_sys import CompLabeledSys, IsPBC
+try:
+    import ase
+except:
+    skip_ase = True
+else:
+    skip_ase = False
 
+@unittest.skipIf(skip_ase,"skip ase related test. install ase to fix")
 class TestASEtraj1(unittest.TestCase, CompLabeledSys, IsPBC):
     def setUp (self) :
-        self.multi_systems = dpdata.MultiSystems.from_file('ase/HeAlO.traj', fmt='ase/structure')
+        self.multi_systems = dpdata.MultiSystems.from_file('ase_traj/HeAlO.traj', fmt='ase_traj/structure')
         self.system_1 = self.multi_systems.systems['Al0He4O0']
-        self.system_2 = dpdata.LabeledSystem('ase/Al0He4O0', fmt='deepmd')
+        self.system_2 = dpdata.LabeledSystem('ase_traj/Al0He4O0', fmt='deepmd')
         self.places = 6
         self.e_places = 6
         self.f_places = 6
         self.v_places = 4
 
+@unittest.skipIf(skip_ase,"skip ase related test. install ase to fix")
 class TestASEtraj1(unittest.TestCase, CompLabeledSys, IsPBC):
     def setUp (self) :
-        self.system_temp0 = dpdata.MultiSystems.from_file(file_name='ase/HeAlO.traj', fmt='ase/structure')
+        self.system_temp0 = dpdata.MultiSystems.from_file(file_name='ase_traj/HeAlO.traj', fmt='ase_traj/structure')
         self.system_1 = self.system_temp0.systems['Al2He1O3'] # .sort_atom_types()
-        self.system_temp1 = dpdata.LabeledSystem('ase/Al2He1O3', fmt='deepmd')
-        self.system_temp2 = dpdata.LabeledSystem('ase/Al4He4O6', fmt='deepmd')
+        self.system_temp1 = dpdata.LabeledSystem('ase_traj/Al2He1O3', fmt='deepmd')
+        self.system_temp2 = dpdata.LabeledSystem('ase_traj/Al4He4O6', fmt='deepmd')
         self.system_temp3 = dpdata.MultiSystems(self.system_temp2, self.system_temp1)
         self.system_2 = self.system_temp3.systems['Al2He1O3']
         self.places = 6
--- tests/test_pymatgen_molecule.py
+++ tests/test_pymatgen_molecule.py
@@ -2,17 +2,24 @@ import os
 import numpy as np
 import unittest
 from context import dpdata
+try:
+    import pymatgen
+except:
+    skip_pymatgen=True
+else:
+    skip_pymatgen=False
 
+@unittest.skipIf(skip_pymatgen,"skip pymatgen related test. install pymatgen to fix")
 class TestPOSCARCart(unittest.TestCase):
     
     def setUp(self): 
         self.system = dpdata.System()
-        self.system.from_pymatgen_molecule(os.path.join('pymatgen', 'FA-001.xyz'))
+        self.system.from_pymatgen_molecule(os.path.join('pymatgen_data', 'FA-001.xyz'))
         self.assertEqual(list(self.system["atom_types"]), [0, 1, 2, 1, 1, 2, 1, 1])
 
     def test_poscar_to_molecule(self):
         tmp_system = dpdata.System()
-        tmp_system.from_vasp_poscar(os.path.join('pymatgen', 'mol2.vasp'))
+        tmp_system.from_vasp_poscar(os.path.join('pymatgen_data', 'mol2.vasp'))
         natoms = len(tmp_system['coords'][0])
         tmpcoord = tmp_system['coords'][0]
         cog = np.average(tmpcoord, axis = 0)
