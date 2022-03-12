3821c9040c9de8c9179c0f3c24ac0684437e04c8
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-03-12 00:25:43 -0500

    add the deepmodeling banner to doc (#254)

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setuptools.setup(
         'ase': ['ase'],
         'amber': ['parmed'],
         'pymatgen': ['pymatgen'],
-        'docs': ['sphinx', 'recommonmark', 'sphinx_rtd_theme>=1.0.0rc1', 'numpydoc', 'm2r2'],
+        'docs': ['sphinx', 'recommonmark', 'sphinx_rtd_theme>=1.0.0rc1', 'numpydoc', 'm2r2', 'deepmodeling-sphinx'],
     }
 )
 
