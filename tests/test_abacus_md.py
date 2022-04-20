8196d9efb8b218c4598b16ae67412a8d37aecc38
Liu Renxi <75369672+Liu-RX@users.noreply.github.com>
2022-04-21 07:13:08 +0800

    adapt dpdata interface to ABACUS 2.2.0, fixed issue#268 about qe/pw/scf (#270)
    * adapt dpgen interface to ABACUS 2.2.0, fixed issue#268 about qe/pw/scf
    
    * Add files via upload
    
    add running_md.log file
    
    * removed deprecated code for old ABACUS MD output
    
    Co-authored-by: LiuRenxi <liurenxi@LiuRenxideMacBook-Pro.local>

--- tests/test_abacus_md.py
+++ tests/test_abacus_md.py
@@ -9,54 +9,52 @@ bohr2ang = LengthConversion("bohr", "angstrom").value()
 class TestABACUSMD:
 
     def test_atom_names(self) :
-        self.assertEqual(self.system_Si.data['atom_names'], ['Si'])
-        #self.assertEqual(self.system_h2o.data['atom_names'], ['O','H'])
+        self.assertEqual(self.system_water.data['atom_names'], ['H', 'O'])
 
     def test_atom_numbs(self) :
-        self.assertEqual(self.system_Si.data['atom_numbs'], [2])
-        #self.assertEqual(self.system_h2o.data['atom_numbs'], [64,128])
+        self.assertEqual(self.system_water.data['atom_numbs'], [2, 1])
 
     def test_atom_types(self) :
-        ref_type = [0, 0]
+        ref_type = [0, 0, 1]
         ref_type =  np.array(ref_type)
         for ii in range(ref_type.shape[0]) :
-            self.assertEqual(self.system_Si.data['atom_types'][ii], ref_type[ii])
+            self.assertEqual(self.system_water.data['atom_types'][ii], ref_type[ii])
 
     def test_cell(self) :
-        cell = bohr2ang * 10.2 * np.array([[0.5, 0.5, 0], [0.5, 0, 0.5], [0, 0.5, 0.5]])
-        for idx in range(np.shape(self.system_Si.data['cells'])[0]):
+        cell = bohr2ang * 28 * np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        for idx in range(np.shape(self.system_water.data['cells'])[0]):
             for ii in range(cell.shape[0]) :
                 for jj in range(cell.shape[1]) :
-                    self.assertAlmostEqual(self.system_Si.data['cells'][idx][ii][jj], cell[ii][jj])
+                    self.assertAlmostEqual(self.system_water.data['cells'][idx][ii][jj], cell[ii][jj])
 
     def test_coord(self) :
-        fp = open('abacus.md/Si_coord')
+        fp = open('abacus.md/water_coord')
         coord = []
         for ii in fp :
             coord.append([float(jj) for jj in ii.split()])
         coord = np.array(coord)
-        coord = coord.reshape([5, 2, 3])
+        coord = coord.reshape([5, 3, 3])
         for ii in range(coord.shape[0]) :
             for jj in range(coord.shape[1]) :
                 for kk in range(coord.shape[2]):
-                    self.assertAlmostEqual(self.system_Si.data['coords'][ii][jj][kk], coord[ii][jj][kk])
+                    self.assertAlmostEqual(self.system_water.data['coords'][ii][jj][kk], coord[ii][jj][kk])
         fp.close()
 
     def test_force(self) :
-        fp = open('abacus.md/Si_force')
+        fp = open('abacus.md/water_force')
         force = []
         for ii in fp :
             force.append([float(jj) for jj in ii.split()])
         force = np.array(force)
-        force = force.reshape([5, 2, 3])
+        force = force.reshape([5, 3, 3])
         for ii in range(force.shape[0]) :
             for jj in range(force.shape[1]) :
                 for kk in range(force.shape[2]):
-                    self.assertAlmostEqual(self.system_Si.data['forces'][ii][jj][kk], force[ii][jj][kk])
+                    self.assertAlmostEqual(self.system_water.data['forces'][ii][jj][kk], force[ii][jj][kk])
         fp.close()
 
     def test_virial(self) :
-        fp = open('abacus.md/Si_virial')
+        fp = open('abacus.md/water_virial')
         virial = []
         for ii in fp :
             virial.append([float(jj) for jj in ii.split()])
@@ -65,21 +63,20 @@ class TestABACUSMD:
         for ii in range(virial.shape[0]) :
             for jj in range(virial.shape[1]) :
                 for kk in range(virial.shape[2]) :
-                    self.assertAlmostEqual(self.system_Si.data['virials'][ii][jj][kk], virial[ii][jj][kk])
+                    self.assertAlmostEqual(self.system_water.data['virials'][ii][jj][kk], virial[ii][jj][kk])
         fp.close()    
 
     def test_energy(self) :
-        ref_energy = np.array([-211.77183266, -211.7739761 , -211.77713677, -211.78079673,
-       -211.78511428])
+        ref_energy = np.array([-466.69285117, -466.69929051, -466.69829826, -466.70364664,
+       -466.6976083])
         for ii in range(5):
-            self.assertAlmostEqual(self.system_Si.data['energies'][ii], ref_energy[ii])
+            self.assertAlmostEqual(self.system_water.data['energies'][ii], ref_energy[ii])
 
 
 class TestABACUSMDLabeledOutput(unittest.TestCase, TestABACUSMD):
 
     def setUp(self):
-        self.system_Si = dpdata.LabeledSystem('abacus.md',fmt='abacus/md')
-        # self.system_h2o = dpdata.LabeledSystem('qe.scf/02.out',fmt='qe/pw/scf')
+        self.system_water = dpdata.LabeledSystem('abacus.md',fmt='abacus/md')
 
 
 if __name__ == '__main__':
