61e1040e09632766720789c0b5abf29f495313c3
LiuRenxi <liurenxi@LiuRenxideMacBook-Pro.local>
2022-06-09 13:46:16 +0800

    add unit test for abacus md without stress output.

--- tests/test_abacus_md.py
+++ tests/test_abacus_md.py
@@ -10,22 +10,32 @@ class TestABACUSMD:
 
     def test_atom_names(self) :
         self.assertEqual(self.system_water.data['atom_names'], ['H', 'O'])
+        self.assertEqual(self.system_Si.data['atom_names'], ['Si'])
 
     def test_atom_numbs(self) :
         self.assertEqual(self.system_water.data['atom_numbs'], [2, 1])
+        self.assertEqual(self.system_Si.data['atom_numbs'], [2])
 
     def test_atom_types(self) :
         ref_type = [0, 0, 1]
         ref_type =  np.array(ref_type)
+        ref_type2 = np.array([0, 0])
         for ii in range(ref_type.shape[0]) :
             self.assertEqual(self.system_water.data['atom_types'][ii], ref_type[ii])
+        for ii in range(ref_type2.shape[0]) :
+            self.assertEqual(self.system_Si.data['atom_types'][ii], ref_type2[ii])
 
     def test_cell(self) :
         cell = bohr2ang * 28 * np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        cell2 = bohr2ang * 5.1 * np.array([[1, 1, 0], [1, 0, 1], [0, 1, 1]])
         for idx in range(np.shape(self.system_water.data['cells'])[0]):
             for ii in range(cell.shape[0]) :
                 for jj in range(cell.shape[1]) :
                     self.assertAlmostEqual(self.system_water.data['cells'][idx][ii][jj], cell[ii][jj])
+        for idx in range(np.shape(self.system_Si.data['cells'])[0]):
+            for ii in range(cell2.shape[0]) :
+                for jj in range(cell2.shape[1]) :
+                    self.assertAlmostEqual(self.system_Si.data['cells'][idx][ii][jj], cell2[ii][jj])
 
     def test_coord(self) :
         fp = open('abacus.md/water_coord')
@@ -40,6 +50,18 @@ class TestABACUSMD:
                     self.assertAlmostEqual(self.system_water.data['coords'][ii][jj][kk], coord[ii][jj][kk])
         fp.close()
 
+        fp2 = open('abacus.md.nostress/Si_coord')
+        coord = []
+        for ii in fp2 :
+            coord.append([float(jj) for jj in ii.split()])
+        coord = np.array(coord)
+        coord = coord.reshape([4, 2, 3])
+        for ii in range(coord.shape[0]) :
+            for jj in range(coord.shape[1]) :
+                for kk in range(coord.shape[2]):
+                    self.assertAlmostEqual(self.system_Si.data['coords'][ii][jj][kk], coord[ii][jj][kk])
+        fp2.close()
+
     def test_force(self) :
         fp = open('abacus.md/water_force')
         force = []
@@ -52,6 +74,18 @@ class TestABACUSMD:
                 for kk in range(force.shape[2]):
                     self.assertAlmostEqual(self.system_water.data['forces'][ii][jj][kk], force[ii][jj][kk])
         fp.close()
+
+        fp2 = open('abacus.md.nostress/Si_force')
+        force = []
+        for ii in fp2 :
+            force.append([float(jj) for jj in ii.split()])
+        force = np.array(force)
+        force = force.reshape([4, 2, 3])
+        for ii in range(force.shape[0]) :
+            for jj in range(force.shape[1]) :
+                for kk in range(force.shape[2]):
+                    self.assertAlmostEqual(self.system_Si.data['forces'][ii][jj][kk], force[ii][jj][kk])
+        fp2.close()
 
     def test_virial(self) :
         fp = open('abacus.md/water_virial')
@@ -69,15 +103,18 @@ class TestABACUSMD:
     def test_energy(self) :
         ref_energy = np.array([-466.69285117, -466.69929051, -466.69829826, -466.70364664,
        -466.6976083])
+        ref_energy2 = np.array([-211.77184603, -211.78111966, -211.79681663, -211.79875524])
         for ii in range(5):
             self.assertAlmostEqual(self.system_water.data['energies'][ii], ref_energy[ii])
+        for ii in range(4):
+            self.assertAlmostEqual(self.system_Si.data['energies'][ii], ref_energy2[ii])
 
 
 class TestABACUSMDLabeledOutput(unittest.TestCase, TestABACUSMD):
 
     def setUp(self):
-        self.system_water = dpdata.LabeledSystem('abacus.md',fmt='abacus/md')
-
+        self.system_water = dpdata.LabeledSystem('abacus.md',fmt='abacus/md') # system with stress
+        self.system_Si = dpdata.LabeledSystem('abacus.md.nostress',fmt='abacus/md') # system without stress
 
 if __name__ == '__main__':
     unittest.main()
