f9397124020e9f1dc969137bffc1a1e65c33b937
LiuRenxi <liurenxi@LiuRenxideMacBook-Pro.local>
2022-06-11 14:02:37 +0800

    add assertion failed reason for abacus/md interface

--- tests/test_abacus_md.py
+++ tests/test_abacus_md.py
@@ -38,67 +38,64 @@ class TestABACUSMD:
                     self.assertAlmostEqual(self.system_Si.data['cells'][idx][ii][jj], cell2[ii][jj])
 
     def test_coord(self) :
-        fp = open('abacus.md/water_coord')
-        coord = []
-        for ii in fp :
-            coord.append([float(jj) for jj in ii.split()])
-        coord = np.array(coord)
-        coord = coord.reshape([5, 3, 3])
-        for ii in range(coord.shape[0]) :
-            for jj in range(coord.shape[1]) :
-                for kk in range(coord.shape[2]):
-                    self.assertAlmostEqual(self.system_water.data['coords'][ii][jj][kk], coord[ii][jj][kk])
-        fp.close()
+        with open('abacus.md/water_coord') as fp:
+            coord = []
+            for ii in fp :
+                coord.append([float(jj) for jj in ii.split()])
+            coord = np.array(coord)
+            coord = coord.reshape([5, 3, 3])
+            for ii in range(coord.shape[0]) :
+                for jj in range(coord.shape[1]) :
+                    for kk in range(coord.shape[2]):
+                        self.assertAlmostEqual(self.system_water.data['coords'][ii][jj][kk], coord[ii][jj][kk])
 
-        fp2 = open('abacus.md.nostress/Si_coord')
-        coord = []
-        for ii in fp2 :
-            coord.append([float(jj) for jj in ii.split()])
-        coord = np.array(coord)
-        coord = coord.reshape([4, 2, 3])
-        for ii in range(coord.shape[0]) :
-            for jj in range(coord.shape[1]) :
-                for kk in range(coord.shape[2]):
-                    self.assertAlmostEqual(self.system_Si.data['coords'][ii][jj][kk], coord[ii][jj][kk])
-        fp2.close()
+        with open('abacus.md.nostress/Si_coord') as fp2:
+            coord = []
+            for ii in fp2 :
+                coord.append([float(jj) for jj in ii.split()])
+            coord = np.array(coord)
+            coord = coord.reshape([4, 2, 3])
+            for ii in range(coord.shape[0]) :
+                for jj in range(coord.shape[1]) :
+                    for kk in range(coord.shape[2]):
+                        self.assertAlmostEqual(self.system_Si.data['coords'][ii][jj][kk], coord[ii][jj][kk])
 
     def test_force(self) :
-        fp = open('abacus.md/water_force')
-        force = []
-        for ii in fp :
-            force.append([float(jj) for jj in ii.split()])
-        force = np.array(force)
-        force = force.reshape([5, 3, 3])
-        for ii in range(force.shape[0]) :
-            for jj in range(force.shape[1]) :
-                for kk in range(force.shape[2]):
-                    self.assertAlmostEqual(self.system_water.data['forces'][ii][jj][kk], force[ii][jj][kk])
-        fp.close()
+        with open('abacus.md/water_force') as fp:
+            force = []
+            for ii in fp :
+                force.append([float(jj) for jj in ii.split()])
+            force = np.array(force)
+            force = force.reshape([5, 3, 3])
+            for ii in range(force.shape[0]) :
+                for jj in range(force.shape[1]) :
+                    for kk in range(force.shape[2]):
+                        self.assertAlmostEqual(self.system_water.data['forces'][ii][jj][kk], force[ii][jj][kk])
 
-        fp2 = open('abacus.md.nostress/Si_force')
-        force = []
-        for ii in fp2 :
-            force.append([float(jj) for jj in ii.split()])
-        force = np.array(force)
-        force = force.reshape([4, 2, 3])
-        for ii in range(force.shape[0]) :
-            for jj in range(force.shape[1]) :
-                for kk in range(force.shape[2]):
-                    self.assertAlmostEqual(self.system_Si.data['forces'][ii][jj][kk], force[ii][jj][kk])
-        fp2.close()
+
+        with open('abacus.md.nostress/Si_force') as fp2:
+            force = []
+            for ii in fp2 :
+                force.append([float(jj) for jj in ii.split()])
+            force = np.array(force)
+            force = force.reshape([4, 2, 3])
+            for ii in range(force.shape[0]) :
+                for jj in range(force.shape[1]) :
+                    for kk in range(force.shape[2]):
+                        self.assertAlmostEqual(self.system_Si.data['forces'][ii][jj][kk], force[ii][jj][kk])
+
 
     def test_virial(self) :
-        fp = open('abacus.md/water_virial')
-        virial = []
-        for ii in fp :
-            virial.append([float(jj) for jj in ii.split()])
-        virial = np.array(virial)
-        virial = virial.reshape([5, 3, 3])
-        for ii in range(virial.shape[0]) :
-            for jj in range(virial.shape[1]) :
-                for kk in range(virial.shape[2]) :
-                    self.assertAlmostEqual(self.system_water.data['virials'][ii][jj][kk], virial[ii][jj][kk])
-        fp.close()    
+        with open('abacus.md/water_virial') as fp:
+            virial = []
+            for ii in fp :
+                virial.append([float(jj) for jj in ii.split()])
+            virial = np.array(virial)
+            virial = virial.reshape([5, 3, 3])
+            for ii in range(virial.shape[0]) :
+                for jj in range(virial.shape[1]) :
+                    for kk in range(virial.shape[2]) :
+                        self.assertAlmostEqual(self.system_water.data['virials'][ii][jj][kk], virial[ii][jj][kk]) 
 
     def test_energy(self) :
         ref_energy = np.array([-466.69285117, -466.69929051, -466.69829826, -466.70364664,
