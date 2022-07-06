1ac64782aecdaa8419961e8fe7ce63ebb9ca243a
HuangJiameng <105633685+HuangJiameng@users.noreply.github.com>
2022-07-06 11:21:25 +0800

    add unittest for aimd_output with stress

--- tests/test_cp2k_aimd_output.py
+++ tests/test_cp2k_aimd_output.py
@@ -15,6 +15,15 @@ class TestCp2kAimdOutput(unittest.TestCase, CompLabeledSys):
         self.f_places = 6
         self.v_places = 4
 
+class TestCp2kAimdStressOutput(unittest.TestCase, CompLabeledSys):
+    def setUp(self):
+        self.system_1 = dpdata.LabeledSystem('cp2k/aimd_stress',fmt='cp2k/aimd_output')
+        self.system_2 = dpdata.LabeledSystem('cp2k/aimd_stress/deepmd', fmt='deepmd/raw')
+        self.places = 6
+        self.e_places = 6
+        self.f_places = 6
+        self.v_places = 4
+
 #class TestCp2kAimdRestartOutput(unittest.TestCase, CompLabeledSys):
 #    def setUp(self):
 #        self.system_1 = dpdata.LabeledSystem('cp2k/restart_aimd',fmt='cp2k/aimd_output', restart=True)
