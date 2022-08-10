87473dbd326fc9d5ea39b13334016e7a36cfac3a
Zezhong Zhang <jack.zezhong.zhang@gmail.com>
2022-08-11 00:30:41 +0200

    let user choose to unwrap and add unittest case

--- dpdata/plugins/lammps.py
+++ dpdata/plugins/lammps.py
@@ -9,7 +9,7 @@ class LAMMPSLmpFormat(Format):
     @Format.post("shift_orig_zero")
     def from_system(self, file_name, type_map=None, **kwargs):
         with open(file_name) as fp:
-            lines = [line.rstrip('\n') for line in fp]
+            lines = [line.rstrip("\n") for line in fp]
         return dpdata.lammps.lmp.to_system_data(lines, type_map)
 
     def to_system(self, data, file_name, frame_idx=0, **kwargs):
@@ -25,9 +25,9 @@ class LAMMPSLmpFormat(Format):
         frame_idx : int
             The index of the frame to dump
         """
-        assert(frame_idx < len(data['coords']))
+        assert frame_idx < len(data["coords"])
         w_str = dpdata.lammps.lmp.from_system_data(data, frame_idx)
-        with open(file_name, 'w') as fp:
+        with open(file_name, "w") as fp:
             fp.write(w_str)
 
 
@@ -35,11 +35,8 @@ class LAMMPSLmpFormat(Format):
 @Format.register("lammps/dump")
 class LAMMPSDumpFormat(Format):
     @Format.post("shift_orig_zero")
-    def from_system(self,
-                    file_name,
-                    type_map=None,
-                    begin=0,
-                    step=1,
-                    **kwargs):
+    def from_system(
+        self, file_name, type_map=None, begin=0, step=1, unwrap=False, **kwargs
+    ):
         lines = dpdata.lammps.dump.load_file(file_name, begin=begin, step=step)
-        return dpdata.lammps.dump.system_data(lines, type_map)
+        return dpdata.lammps.dump.system_data(lines, type_map, unwrap=unwrap)
--- tests/test_lammps_dump_unfold.py
+++ tests/test_lammps_dump_unfold.py
@@ -1,26 +1,38 @@
+from inspect import unwrap
 import os
 import numpy as np
 import unittest
 from context import dpdata
-from poscars.poscar_ref_oh import TestPOSCARoh        
+from poscars.poscar_ref_oh import TestPOSCARoh
+
 
 class TestDump(unittest.TestCase, TestPOSCARoh):
-    
-    def setUp(self): 
-        self.system = dpdata.System(os.path.join('poscars', 'conf_unfold.dump'), 
-                                    type_map = ['O', 'H'])
-        
+    def setUp(self):
+        self.system = dpdata.System(
+            os.path.join("poscars", "conf_unfold.dump"), type_map=["O", "H"]
+        )
+
+
 class TestDump2(unittest.TestCase, TestPOSCARoh):
-    
-    def setUp(self): 
-        self.tmp_system = dpdata.System(os.path.join('poscars', 'conf_unfold.dump'), 
-                                        type_map = ['O', 'H'])
+    def setUp(self):
+        self.tmp_system = dpdata.System(
+            os.path.join("poscars", "conf_unfold.dump"), type_map=["O", "H"]
+        )
         self.system = self.tmp_system.sub_system([1])
 
-    def test_nframes (self) :
+    def test_nframes(self):
         self.assertEqual(self.tmp_system.get_nframes(), 2)
-        
-        
-if __name__ == '__main__':
+
+
+class TestDumpUnwrap(unittest.TestCase, TestPOSCARoh):
+    def setUp(self):
+        self.unwrap = True
+        self.system = dpdata.System(
+            os.path.join("poscars", "conf_unfold.dump"),
+            type_map=["O", "H"],
+            unwrap=self.unwrap,
+        )
+
+
+if __name__ == "__main__":
     unittest.main()
-    
