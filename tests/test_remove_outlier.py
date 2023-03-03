7c9a3143381d5299bec88b69563379e016873863
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-03-03 16:41:27 -0500

    add a method to remove outlier frames
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

---
+++ tests/test_remove_outlier.py
@@ -0,0 +1,31 @@
+import os
+import unittest
+
+import numpy as np
+from context import dpdata
+from comp_sys import CompLabeledSys
+
+
+class TestRemoveOutlier(unittest.TestCase, CompLabeledSys):
+    @classmethod
+    def setUpClass(cls):
+        system = dpdata.LabeledSystem(
+            data={
+                "atom_names": ["H"],
+                "atom_numbs": [1],
+                "atom_types": np.zeros((1,), dtype=int),
+                "coords": np.zeros((100, 1, 3), dtype=np.float32),
+                "cells": np.zeros((100, 3, 3), dtype=np.float32),
+                "orig": np.zeros(3, dtype=np.float32),
+                "nopbc": True,
+                "energies": np.zeros((100,), dtype=np.float32),
+                "forces": np.zeros((100, 1, 3), dtype=np.float32),
+            }
+        )
+        system.data['energies'][0] = 100.0
+        cls.system_1 = system.remove_outlier()
+        cls.system_2 = system[1:]
+        cls.places = 6
+        cls.e_places = 6
+        cls.f_places = 6
+        cls.v_places = 6
