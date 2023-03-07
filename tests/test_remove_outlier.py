010af5495f3f25c7affe7c624f9309b82b961fd4
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-03-07 15:11:15 -0500

    test std=0
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- tests/test_remove_outlier.py
+++ tests/test_remove_outlier.py
@@ -29,3 +29,27 @@ class TestRemoveOutlier(unittest.TestCase, CompLabeledSys):
         cls.e_places = 6
         cls.f_places = 6
         cls.v_places = 6
+
+
+class TestRemoveOutlierStdZero(unittest.TestCase, CompLabeledSys):
+    @classmethod
+    def setUpClass(cls):
+        system = dpdata.LabeledSystem(
+            data={
+                "atom_names": ["H"],
+                "atom_numbs": [1],
+                "atom_types": np.zeros((1,), dtype=int),
+                "coords": np.zeros((100, 1, 3), dtype=np.float32),
+                "cells": np.zeros((100, 3, 3), dtype=np.float32),
+                "orig": np.zeros(3, dtype=np.float32),
+                "nopbc": True,
+                "energies": np.zeros((100,), dtype=np.float32),
+                "forces": np.zeros((100, 1, 3), dtype=np.float32),
+            }
+        )
+        cls.system_1 = system.remove_outlier()
+        cls.system_2 = system
+        cls.places = 6
+        cls.e_places = 6
+        cls.f_places = 6
+        cls.v_places = 6
