cb6a0fc7473407a919eb5c25cf44b9b5a8ae196a
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-04-10 20:57:34 -0400

    prevent F401
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- tests/context.py
+++ tests/context.py
@@ -2,3 +2,18 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import dpdata
+import dpdata.gaussian.gjf
+import dpdata.md.msd
+import dpdata.md.water
+import dpdata.stat
+import dpdata.system
+
+__all__ = [
+    "dpdata",
+    "dpdata.gaussian.gjf",
+    "dpdata.md.msd",
+    "dpdata.md.water",
+    "dpdata.stat",
+    "dpdata.system",
+]
