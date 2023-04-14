a1478bdcf8386ea748c69e0bcbd028bfa13a45bf
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-04-14 17:23:44 -0400

    add tests

--- tests/test_gaussian_gjf.py
+++ tests/test_gaussian_gjf.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 from context import dpdata
+from comp_sys import CompSys
 
 
 class TestGaussianGJF(unittest.TestCase):
@@ -11,3 +12,12 @@ class TestGaussianGJF(unittest.TestCase):
     def test_dump_gaussian_gjf(self):
         self.system.to_gaussian_gjf("tmp.gjf", keywords="force b3lyp/6-31g*")
         os.remove("tmp.gjf")
+
+
+class TestGaussianGJFComp(unittest.TestCase, CompSys):
+    def setUp(self):
+        self.system_1 = dpdata.LabeledSystem("poscars/OUTCAR.h2o.md", fmt="vasp/outcar")
+        self.system_2.to_gaussian_gjf("tmp.gjf", keywords="force b3lyp/6-31g*")
+        self.system_2 = dpdata.System("tmp.gjf", fmt="gaussian/gjf")
+        os.remove("tmp.gjf")
+        self.places = 6
