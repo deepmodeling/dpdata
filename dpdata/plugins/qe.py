735126f725fdd4ea9e4a74884c4f146032407a4d
pee8379 <pee8379@users.noreply.github.com>
2023-05-18 17:09:34 +0900

    Update qe.py
    Signed-off-by: pee8379 <pee8379@users.noreply.github.com>

--- dpdata/plugins/qe.py
+++ dpdata/plugins/qe.py
@@ -1,6 +1,7 @@
 import dpdata.md.pbc
 import dpdata.qe.scf
 import dpdata.qe.traj
+import dpdata.qe.pwmd
 from dpdata.format import Format
 
 
@@ -49,3 +50,25 @@ class QECPPWSCFFormat(Format):
             data["virials"],
         ) = dpdata.qe.scf.get_frame(file_name)
         return data
+
+
+@Format.register("qe/pw/md")
+class QECPPWMDFormat(Format):
+	@Format.post("rot_lower_triangular")
+	def from_labeled_system(self, file_name, begin=0, step=1, **kwargs):
+		data = {}
+		(
+			data["atom_names"],
+			data["atom_numbs"],
+			data["atom_types"],
+			data["cells"],
+			data["coords"],
+			data["energies"],
+			data["forces"],
+			data["virials"],
+		) = dpdata.qe.pwmd.to_system_data(file_name, begin=begin, step=step)
+		data["coords"] = dpdata.md.pbc.apply_pbc(
+            data["coords"],
+            data["cells"],
+        )
+		return data
