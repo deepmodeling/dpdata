be776038b2600b78820805faf1c1c73eb334036a
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-05-23 17:15:45 -0400

    allow custom data for deepmd format
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

---
+++ tests/test_custom_data_type.py
@@ -0,0 +1,53 @@
+import unittest
+
+import numpy as np
+import h5py
+
+import dpdata
+from dpdata.system import Axis, DataType
+
+class TestDeepmdLoadDumpComp(unittest.TestCase):
+    def setUp(self):
+        self.backup = dpdata.system.LabeledSystem.DTYPES
+        dpdata.system.LabeledSystem.DTYPES = dpdata.system.LabeledSystem.DTYPES + (
+            DataType("foo", np.ndarray, (Axis.NFRAMES, 2, 4), required=False),
+        )
+        self.system = dpdata.LabeledSystem("poscars/OUTCAR.h2o.md", fmt="vasp/outcar")
+        self.foo = np.ones((len(self.system), 2, 4))
+        self.system.data["foo"] = self.foo
+        self.system.check_data()
+
+    def tearDown(self) -> None:
+        dpdata.system.LabeledSystem.DTYPES = self.backup
+
+    def test_to_deepmd_raw(self):
+        self.system.to_deepmd_raw("data_foo")
+        foo = np.loadtxt("data_foo/foo.raw")
+        np.testing.assert_allclose(foo.reshape(self.foo.shape), self.foo)
+
+    def test_from_deepmd_raw(self):
+        self.system.to_deepmd_raw("data_foo")
+        x = dpdata.LabeledSystem("data_foo", fmt="deepmd/raw")
+        np.testing.assert_allclose(x.data["foo"], self.foo)
+
+    def test_to_deepmd_npy(self):
+        self.system.to_deepmd_npy("data_foo")
+        foo = np.load("data_foo/set.000/foo.npy")
+        np.testing.assert_allclose(foo.reshape(self.foo.shape), self.foo)
+
+    def test_from_deepmd_npy(self):
+        self.system.to_deepmd_npy("data_foo")
+        x = dpdata.LabeledSystem("data_foo", fmt="deepmd/npy")
+        np.testing.assert_allclose(x.data["foo"], self.foo)
+
+    def test_to_deepmd_hdf5(self):
+        self.system.to_deepmd_hdf5("data_foo.h5")
+        with h5py.File("data_foo.h5") as f:
+            foo = f["set.000/foo.npy"][:]
+        #foo = np.load("data_foo/set.000/foo.npy")
+        np.testing.assert_allclose(foo.reshape(self.foo.shape), self.foo)
+
+    def test_from_deepmd_hdf5(self):
+        self.system.to_deepmd_hdf5("data_foo.h5")
+        x = dpdata.LabeledSystem("data_foo.h5", fmt="deepmd/hdf5")
+        np.testing.assert_allclose(x.data["foo"], self.foo)
