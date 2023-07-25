0b9c5365a6711b17c9b4edd286b96d4ae0c350db
L-RuiHao <874800796@qq.com>
2023-07-25 16:12:56 +0800

    new feature dftb_plus

--- dpdata/dftb_plus/inout.py
+++ dpdata/dftb_plus/inout.py
@@ -3,7 +3,7 @@ import numpy as np
 from typing import Tuple
 
 
-def read_dftb_plus(fn: str) -> Tuple[str, np.ndarray, float, np.ndarray]: #两个文件作为输入，一个得到symbols和coord ,另一个得到ener
+def read_dftb_plus(fn: str) -> Tuple[str, np.ndarray, float, np.ndarray]: 
     with open(fn) as f:
         flag = 0
         for line in f:
@@ -13,18 +13,17 @@ def read_dftb_plus(fn: str) -> Tuple[str, np.ndarray, float, np.ndarray]: #两�
                 components = line.split() 
                 flag +=1
             elif line.startswith("Geometry"):
-                # coord symbol
                 flag = 1
                 coord = []
                 symbols = []  
             elif flag in (3,4,5,6): 
                 s = line.split()  
                 components_num = int(s[1]) 
-                symbols.append(components[components_num-1]) #得到元素标志符号
+                symbols.append(components[components_num-1]) 
                 coord.append([float(s[2]), float(s[3]), float(s[4])])
                 flag +=1
                 if flag == 7:
-                    flag = 0  #结束对坐标的读取  
+                    flag = 0  
             elif line.startswith("Total Forces"):
                 flag = 8
                 forces = []
@@ -33,7 +32,7 @@ def read_dftb_plus(fn: str) -> Tuple[str, np.ndarray, float, np.ndarray]: #两�
                 forces.append([float(s[1]), float(s[2]), float(s[3])])
                 flag +=1
                 if flag == 12:
-                    flag = 0  #结束对力的读取
+                    flag = 0  
             elif line.startswith("Total energy:"):
                 s = line.split() 
                 energy = float(s[2])
