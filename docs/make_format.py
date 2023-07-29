7392eb2252bd8ae00a25a89e54a6114b4a8ef505
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-07-28 21:50:08 -0400

    skip
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- docs/make_format.py
+++ docs/make_format.py
@@ -285,7 +285,9 @@ def generate_sub_format_pages(formats: dict):
                     )
                     buff.append("""   :noindex:""")
                 buff.append("")
-                if docstring is None or method not in format.__dict__:
+                if method == "to_labeled_system" and method not in format.__dict__ and "to_system" in format.__dict__:
+                    pass
+                elif docstring is None or method not in format.__dict__:
                     docstring = "Convert :class:`%s` to this format." % (
                         method_classes[method]
                     )
