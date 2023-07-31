049827fe63750c35513410a5c3e809485f0990a0
L-RuiHao <874800796@qq.com>
2023-07-31 22:50:40 +0800

    new modification

--- dpdata/plugins/dftbplus.py
+++ dpdata/plugins/dftbplus.py
@@ -1,17 +1,44 @@
 import numpy as np
 
-from dpdata.dftb_plus.output import read_dftb_plus
+from dpdata.dftbplus.output import read_dftb_plus
 from dpdata.format import Format
 from dpdata.unit import EnergyConversion, ForceConversion, LengthConversion
 
-length_convert = LengthConversion("bohr", "angstrom").value()
 energy_convert = EnergyConversion("hartree", "eV").value()
 force_convert = ForceConversion("hartree/bohr", "eV/angstrom").value()
 
 
 @Format.register("dftb_plus")
 class DFTBplusFormat(Format):
+    """
+    The DFTBplusFormat class handles files in the DFTB+ format.
+
+    This class provides a method to read DFTB+ files from a labeled system and
+    returns a dictionary containing various properties of the system.For more 
+    information, please refer to the official documentation at the following URL:
+    https://dftbplus.org/documentation
+
+    Attributes:
+        None
+
+    Methods:
+        from_labeled_system(file_paths, **kwargs): Reads system information from files.
+
+        
+    """
+        
     def from_labeled_system(self, file_paths, **kwargs):
+        """
+        Reads system information from the given DFTB+ file paths.
+
+        Parameters:
+            file_paths (tuple): A tuple containing the input and output file paths.
+            the input file path should be a .hsd file and the output file path should be a .out file.
+                - Input file (file_in): Contains information about symbols and coord.
+                - Output file (file_out): Contains information about energy and force.
+
+            **kwargs: Additional optional keyword arguments.
+        """
         file_in, file_out = file_paths
         symbols, coord, energy, forces = read_dftb_plus(file_in,file_out)
         last_occurrence = {v: i for i, v in enumerate(symbols)}
@@ -24,7 +51,7 @@ class DFTBplusFormat(Format):
             "atom_types": atom_types,
             "atom_names": list(atom_names),
             "atom_numbs": list(atom_numbs),
-            "coords": (coord * length_convert).reshape((1, natoms, 3)),
+            "coords": coord.reshape((1, natoms, 3)),
             "energies": np.array([energy * energy_convert]),
             "forces": (forces * force_convert).reshape((1, natoms, 3)),
             "cells": np.zeros((1, 3, 3)),
