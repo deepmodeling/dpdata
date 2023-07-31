22bc851f8f982e997af8ae966927ebd030c7829c
L-RuiHao <874800796@qq.com>
2023-07-31 11:37:10 +0800

    New Modification

--- dpdata/plugins/dftb_plus.py
+++ dpdata/plugins/dftb_plus.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from dpdata.dftb_plus.inout import read_dftb_plus
+from dpdata.dftb_plus.output import read_dftb_plus
 from dpdata.format import Format
 from dpdata.unit import EnergyConversion, ForceConversion, LengthConversion
 
@@ -11,8 +11,9 @@ force_convert = ForceConversion("hartree/bohr", "eV/angstrom").value()
 
 @Format.register("dftb_plus")
 class DFTBplusFormat(Format):
-    def from_labeled_system(self, file_name, **kwargs):
-        symbols, coord, energy, forces = read_dftb_plus(file_name)
+    def from_labeled_system(self, file_paths, **kwargs):
+        file_in, file_out = file_paths
+        symbols, coord, energy, forces = read_dftb_plus(file_in,file_out)
         last_occurrence = {v: i for i, v in enumerate(symbols)}
         atom_names = np.array(sorted(np.unique(symbols), key=last_occurrence.get))
         atom_types = np.array([np.where(atom_names == s)[0][0] for s in symbols])
@@ -30,3 +31,8 @@ class DFTBplusFormat(Format):
             "orig": np.zeros(3),
             "nopbc": True,
         }
+
+        
+
+
+        
