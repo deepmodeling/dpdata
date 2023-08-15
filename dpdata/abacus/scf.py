61ad06387ce3df2a9fd04a64a4cece2f3f5715ff
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-08-15 16:13:19 -0400

    fix typo

--- dpdata/abacus/scf.py
+++ dpdata/abacus/scf.py
@@ -255,7 +255,7 @@ def get_nele_from_stru(geometry_inlines):
 
 
 def get_frame_from_stru(fname):
-    assert isinstacne(fname, str)
+    assert isinstance(fname, str)
     with open(fname) as fp:
         geometry_inlines = fp.read().split("\n")
     nele = get_nele_from_stru(geometry_inlines)
