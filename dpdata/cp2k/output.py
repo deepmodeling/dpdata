edaa946f97928deb750b662c51961f73e4f3becc
link89 <xuweihong.cn@gmail.com>
2023-09-05 00:42:31 +0800

    cp2k output: handle empty line properly
    Signed-off-by: link89 <xuweihong.cn@gmail.com>

--- dpdata/cp2k/output.py
+++ dpdata/cp2k/output.py
@@ -410,17 +410,14 @@ def get_frames(fname):
             if "Atomic kind:" in ii:
                 atom_symbol_list.append(ii.split()[3])
 
-            # parse coords
-            if "Atom  Kind  Element" in ii:  # ver. < 2023
+            # start to parse coords block
+            if "Atom  Kind  Element" in ii or "Atom Kind Element" in ii: 
                 coord_flag = True
-                coord_idx = idx + 1  # skip empty line
-            elif "Atom Kind Element" in ii:  # ver. >= 2023
-                coord_flag = True
-                coord_idx = idx
-            # get the coord block info
+                coord_idx = idx  # save start line of coord block 
+            # parse coords lines 
             if coord_flag and idx > coord_idx:
                 if ii == "\n":
-                    coord_flag = False
+                    coord_flag = len(coord) == 0  # skip empty line at the beginning
                 else:
                     coord.append(ii.split()[4:7])
                     atom_symbol_idx_list.append(ii.split()[1])
