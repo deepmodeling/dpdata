56cb8f71186f4307756933c8e640bc77d8ac6374
Uper <41718895+Hyliu-BUAA@users.noreply.github.com>
2024-01-22 18:05:44 +0800

    Add test for new code

--- tests/test_pwmat_movement.py
+++ tests/test_pwmat_movement.py
@@ -58,14 +58,74 @@ class TestpwmatSinglePointEnergy:
         self.assertEqual(self.system.data["energies"][0], ref_energy)
 
 
+class TestpwmatSinglePointEnergy1:
+    def test_atom_names(self):
+        self.assertEqual(self.system.data["atom_names"], ["Cu"])
+
+    def test_atom_numbs(self):
+        self.assertEqual(self.system.data["atom_numbs"], [72])
+
+    def test_atom_types(self):
+        ref_type = [0] * 72
+        ref_type = np.array(ref_type)
+        for ii in range(ref_type.shape[0]):
+            self.assertEqual(self.system.data["atom_types"][ii], ref_type[ii])
+
+    def test_cell(self):
+        fp = open("pwmat/ref_cell_1")
+        cell = []
+        for ii in fp:
+            cell.append([float(jj) for jj in ii.split()])
+        cell = np.array(cell)
+        for ii in range(cell.shape[0]):
+            for jj in range(cell.shape[1]):
+                self.assertEqual(self.system.data["cells"][0][ii][jj], cell[ii][jj])
+        fp.close()
+    
+    def test_coord(self):
+        fp = open("pwmat/ref_coord_1")
+        fp_cell = open("pwmat/ref_cell_1")
+        cell = []
+        for ii in fp_cell:
+            cell.append([float(jj) for jj in ii.split()])
+        cell = np.array(cell)
+        coord = []
+        for ii in fp:
+            coord.append([float(jj) for jj in ii.split()])
+        coord = np.array(coord)
+        coord = np.matmul(coord, cell)
+        for ii in range(coord.shape[0]):
+            for jj in range(coord.shape[1]):
+                self.assertEqual(
+                    self.system.data["coords"][0][ii][jj], coord[ii][jj]
+                )
+        fp_cell.close()
+        fp.close()
+    
+    def test_force(self):
+        fp = open("pwmat/ref_force_1")
+        force = []
+        for ii in fp:
+            force.append([float(jj) for jj in ii.split()])
+        force = np.array(force)
+        for ii in range(force.shape[0]):
+            for jj in range(force.shape[1]):
+                self.assertEqual(self.system.data["forces"][0][ii][jj], -force[ii][jj])
+        fp.close()
+
+    def test_energy(self):
+        ref_energy = -0.3576828045E+06
+        self.assertEqual(self.system.data["energies"][0], ref_energy)
+
+
 class TestpwmatLabeledOutput(unittest.TestCase, TestpwmatSinglePointEnergy):
     def setUp(self):
         self.system = dpdata.LabeledSystem("pwmat/MOVEMENT", fmt="pwmat/MOVEMENT")
 
 
-class TestpwmatLabeledOutput1(unittest.TestCase):
+class TestpwmatLabeledOutput1(unittest.TestCase, TestpwmatSinglePointEnergy1):
     def setUp(self):
-        self.system = dpdata.LabeledSystem("pwmat/MOVEMENT_1", fmt="pwmat/MOVEMENT_1")
+        self.system = dpdata.LabeledSystem("pwmat/MOVEMENT_1", fmt="pwmat/movement")
 
 
 if __name__ == "__main__":
