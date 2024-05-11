89fe364531f43c90c7bc5e11eff5260782bd3819
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-05-10 21:09:50 -0400

    benchmark performance of import
    Do a benchmark before working on #522
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

---
+++ tests/benchmark/test_import.py
@@ -0,0 +1,13 @@
+import pytest
+import subprocess
+import sys
+
+@pytest.mark.benchmark
+def test_import():
+    """Test import dpdata."""
+    subprocess.check_output([sys.executable, "-c", "'from dpdata import LabeledSystem'"]).decode("ascii")
+
+@pytest.mark.benchmark
+def test_cli():
+    """Test dpdata command."""
+    subprocess.check_output([sys.executable, "-m", "dpdata", "-h"]).decode("ascii")
