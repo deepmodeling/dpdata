fdc5d3ff547b00feb52b6455d54ac5c75e5c881f
Renguang <493282613@qq.com>
2024-05-20 21:18:10 +0800

    Update test_abacus_deltaspin.py

--- tests/test_abacus_deltaspin.py
+++ tests/test_abacus_deltaspin.py
@@ -1,16 +1,16 @@
-from context import dpdata
+# from context import dpdata
 
 
-class TestDeltaSpin(unittest.TestCase):
-    """Make a test for the DeltaSpin module in ABACUS"""
+# class TestDeltaSpin(unittest.TestCase):
+#     """Make a test for the DeltaSpin module in ABACUS"""
 
-    def OUTABACUS_to_system(self):
-        data = dpdata.LabeledSystem("./abacus.scf.deltaspin", fmt="abacus/scf")
-        print(data)
+#     def OUTABACUS_to_system(self):
+#         data = dpdata.LabeledSystem("./abacus.scf.deltaspin", fmt="abacus/scf")
+#         print(data)
 
-    def system_to_npy(self):
-        data = dpdata.LabeledSystem(
-            "./abacus.scf.deltaspin", fmt="abacus/scf"
-        ).to_deepmd_npy("./abacus.scf.deltaspin/")
-        print(data)
+#     def system_to_npy(self):
+#         data = dpdata.LabeledSystem(
+#             "./abacus.scf.deltaspin", fmt="abacus/scf"
+#         ).to_deepmd_npy("./abacus.scf.deltaspin/")
+#         print(data)
 
