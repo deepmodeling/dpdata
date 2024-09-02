f16687ed512788fb34066d1556dfae64897ed828
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-09-02 02:56:41 -0400

    fix tests
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- tests/test_read_file.py
+++ tests/test_read_file.py
@@ -4,19 +4,19 @@ import io
 import unittest
 from pathlib import Path
 
-from dpdata.utils import read_text_file
+from dpdata.utils import open_file
 
 
 class TestReadFile(unittest.TestCase):
-    def test_read_text_file_from_string_io(self):
+    def test_open_file_from_string_io(self):
         string_io = io.StringIO("Hello, world!")
-        with read_text_file(string_io) as file:
+        with open_file(string_io) as file:
             self.assertEqual(file.read(), "Hello, world!")
 
-    def test_read_text_file_from_file_str(self):
-        with read_text_file("/proc/cpuinfo") as file:
+    def test_open_file_from_file_str(self):
+        with open_file("/proc/cpuinfo") as file:
             self.assertEqual(file.read(), Path("/proc/cpuinfo").read_text())
 
-    def test_read_text_file_from_file_path(self):
-        with read_text_file(Path("/proc/cpuinfo")) as file:
+    def test_open_file_from_file_path(self):
+        with open_file(Path("/proc/cpuinfo")) as file:
             self.assertEqual(file.read(), Path("/proc/cpuinfo").read_text())
