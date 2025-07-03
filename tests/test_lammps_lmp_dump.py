a9fc3f8a8bab8531ca34489cd3d3d776179eabc3
root <pxlxingliang>
2025-07-03 15:03:13 +0800

    add UT

--- tests/test_lammps_lmp_dump.py
+++ tests/test_lammps_lmp_dump.py
@@ -5,7 +5,8 @@ import unittest
 
 from context import dpdata
 from poscars.poscar_ref_oh import TestPOSCARoh
-
+from dpdata.lammps.lmp import rotate_to_lower_triangle
+import numpy as np
 
 class TestLmpDump(unittest.TestCase, TestPOSCARoh):
     def setUp(self):
@@ -26,6 +27,66 @@ class TestToFunc(unittest.TestCase, TestPOSCARoh):
         self.system = dpdata.System()
         self.system.from_fmt("tmp.lmp", fmt="lammps/lmp", type_map=["O", "H"])
 
+
+class TestLmpRotateTriAngle(unittest.TestCase):
+    
+    def test_simple_cubic(self):
+        cubic_cell = np.diag([5, 5, 5])
+        cubic_coords = np.array([[0, 0, 0], [2.5, 2.5, 2.5]])
+        rotated_cell, rotated_coords = rotate_to_lower_triangle(cubic_cell, cubic_coords)
+
+        assert np.allclose(rotated_cell, np.diag([5, 5, 5]))
+        assert np.allclose(rotated_coords, cubic_coords)
+    
+    def test_triclinic_cell(self):
+        triclinic_cell = np.array([
+            [4.0, 0.0, 0.0],
+            [1.0, 3.0, 0.0],
+            [0.5, 0.5, 2.5]
+        ])
+
+        triclinic_coords = np.array([
+            [0.5, 0.5, 0.5], 
+            [3.0, 2.0, 1.0]  
+        ])
+        rotated_cell, rotated_coords = rotate_to_lower_triangle(triclinic_cell.copy(), triclinic_coords.copy())
+
+        assert np.isclose(rotated_cell[0, 1], 0, atol=1e-10)
+        assert np.isclose(rotated_cell[0, 2], 0, atol=1e-10)
+        assert np.isclose(rotated_cell[1, 2], 0, atol=1e-10)
+
+        assert rotated_cell[0, 0] > 0
+        assert rotated_cell[1, 1] > 0
+        assert rotated_cell[2, 2] > 0
+
+        # check the distance between two atoms
+        assert np.isclose(np.linalg.norm(rotated_coords[0] - rotated_coords[1]), 
+                          np.linalg.norm(triclinic_coords[0] - triclinic_coords[1]), 
+                          atol=1e-10)
+    
+    def test_negative_diagonal(self):
+        negative_cell = np.array([
+            [-3.0, 1.0, 0.5],
+            [0.0, -2.0, 0.3],
+            [0.0, 0.0, 4.0]
+            ])
+        negative_coords = np.array([[1.0, 0.5, 1.0], [0.5, 1.0, 2.0]])
+        rotated_cell, rotated_coords = rotate_to_lower_triangle(negative_cell.copy(), negative_coords.copy())
+
+        assert np.isclose(rotated_cell[0, 1], 0, atol=1e-10)
+        assert np.isclose(rotated_cell[0, 2], 0, atol=1e-10)
+        assert np.isclose(rotated_cell[1, 2], 0, atol=1e-10)
+        
+        assert rotated_cell[0, 0] > 0
+        assert rotated_cell[1, 1] > 0
+        assert rotated_cell[2, 2] > 0
+
+        assert np.isclose(np.linalg.norm(negative_coords[0] - negative_coords[1]), 
+                          np.linalg.norm(rotated_coords[0] - rotated_coords[1]), 
+                          atol=1e-6)
+    
+        
+        
 
 if __name__ == "__main__":
     unittest.main()
