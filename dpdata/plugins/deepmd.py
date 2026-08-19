b3c69fea619058c7af335b4ce29e8a75431000aa
Duo <50307526+iProzd@users.noreply.github.com>
2026-08-19 23:22:11 +0800

    Raise error when from_deepmd_npy_mixed with not-a-directory para. (#432)
    Signed-off-by: Jinzhe Zeng <njzjz@qq.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Jinzhe Zeng <njzjz@qq.com>
    Co-authored-by: A bot of @njzjz <48687836+njzjz-bot@users.noreply.github.com>

--- dpdata/plugins/deepmd.py
+++ dpdata/plugins/deepmd.py
@@ -204,6 +204,8 @@ class DeePMDMixedFormat(Format):
     def from_multi_systems(self, directory, **kwargs):
         register_spin()
         sys_dir = []
+        if not os.path.isdir(directory):
+            raise FileNotFoundError(f"{directory} is not a directory!")
         for root, dirs, files in os.walk(directory):
             if (
                 "type_map.raw" in files
